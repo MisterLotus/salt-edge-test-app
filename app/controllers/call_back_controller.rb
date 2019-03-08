d1727d3acef19e58baa08605196846aa7782af2e
Alexandr Nicolaev <devever@rambler.ru>
2019-03-08 11:38:06 +0200

    json laout for callback

--- app/controllers/call_back_controller.rb
+++ app/controllers/call_back_controller.rb
@@ -1,4 +1,6 @@
 class CallBackController < ApplicationController
+  # no need side menu or something else, pure json in view
+  layout false
   def handle
       @log = Log.new
       @log.timestamp = DateTime.now.strftime('%Y-%m-%d %H:%M:%S')
